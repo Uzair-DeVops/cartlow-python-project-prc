36a9afb8ba0e469b1535bacdf73223704788e73a
Uzair-DeVops <uzairyasin83@gmail.com>
2025-07-30 23:10:58 +0500

    added  comments

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ Run this from the project root directory
 from app import app
 import uvicorn
 from app.config.my_settings import settings
-
+# test
 
 if __name__ == "__main__":
     uvicorn.run("run:app", host="0.0.0.0", port=int(settings.PORT)) 
