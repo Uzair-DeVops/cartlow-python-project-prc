6b237ec84f54293bcc50a395660fa169e77e2bee
Uzair-DeVops <uzairyasin83@gmail.com>
2025-07-30 19:44:18 +0500

    remove type error

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ import uvicorn
 from app.config.my_settings import settings
 
 if __name__ == "__main__":
-    uvicorn.run("run:app", host="0.0.0.0", port=settings.PORT) 
+    uvicorn.run("run:app", host="0.0.0.0", port=int(settings.PORT)) 
